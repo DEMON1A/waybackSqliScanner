79e8451a6234bfa7af90e5c6550b9a6e30d60dd8
Mohamed Dief <mdaif1332@gmail.com>
2021-02-06 00:21:06 +0200

    Uploading The Program Files.

---
+++ main.py
@@ -0,0 +1,54 @@
+import queue
+import threading
+import sys
+
+from sqliscanner import sqliScannerClass
+from waybackmachine import waybackMachineClass
+
+from colorama import init 
+from termcolor import colored
+init()
+
+
+class sqliWaybackClass():
+
+    def __init__(self):
+        self.urls = []
+        self.q = queue.Queue()
+
+    def getWaybackUrls(self,domain):
+        self.urls = []
+        wbm = waybackMachineClass(domain)
+        waybackJson = wbm.getUrls()
+        for row in waybackJson:
+            self.urls.append(row[0])
+        
+        print(colored("WaybackUrls Has Been Collected. Scanning Them.\n\n" , 'red' , attrs=['bold']))
+
+    def checkSqli(self,url):
+        ss = sqliScannerClass()
+        ss.createSqliUrl(url)
+
+    def worker(self):
+        while 1:
+            url = self.q.get()
+            self.checkSqli(url)
+            self.q.task_done()
+
+    def run(self,domain):
+
+        self.getWaybackUrls(domain)
+
+        #Spin up workers
+        for i in range(50):
+            t = threading.Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+        for url in self.urls:
+            self.q.put(url)
+        self.q.join()
+
+
+sw = sqliWaybackClass()
+sw.run(sys.argv[1])
---
+++ sqliscanner.py
@@ -0,0 +1,71 @@
+import requests, json, re
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+from colorama import init 
+from termcolor import colored
+init()
+
+class sqliScannerClass():
+    def __init__(self):
+        # SQL Error Messages
+        self.MySQL = ["SQL syntax.*MySQL", "Warning.*mysql_.*", "valid MySQL result", "MySqlClient\."]
+        self.PostgreSQL = ["PostgreSQL.*ERROR", "Warning.*\Wpg_.*", "valid PostgreSQL result", "Npgsql\."]
+        self.MicrosoftSQLServer = ["Driver.* SQL[\-\_\ ]*Server", "OLE DB.* SQL Server", "(\W|\A)SQL Server.*Driver", "Warning.*mssql_.*", "(\W|\A)SQL Server.*[0-9a-fA-F]{8}", "(?s)Exception.*\WSystem\.Data\.SqlClient\.", "(?s)Exception.*\WRoadhouse\.Cms\."]
+        self.MicrosoftAccess = ["Microsoft Access Driver", "JET Database Engine", "Access Database Engine"]
+        self.Oracle = ["\bORA-[0-9][0-9][0-9][0-9]", "Oracle error", "Oracle.*Driver", "Warning.*\Woci_.*", "Warning.*\Wora_.*"]
+        self.IBMDB2 = ["CLI Driver.*DB2", "DB2 SQL error", "\bdb2_\w+\("]
+        self.SQLite = ["SQLite/JDBCDriver", "SQLite.Exception", "System.Data.SQLite.SQLiteException", "Warning.*sqlite_.*", "Warning.*SQLite3::", "\[SQLITE_ERROR\]"]
+        self.Sybase = ["(?i)Warning.*sybase.*", "Sybase message", "Sybase.*Server message.*"]
+
+    def createSqliUrl(self,url):
+        try:
+            urlParams = url.split("?")[1] 
+        except:
+            return
+
+        urlParams = urlParams.split("&")
+        for param in urlParams:
+            try:
+                param = param.split("=")
+                paramStr = str(param[0]+"="+param[1])
+
+                #Check for sqli using '
+                paramExploitStr = paramStr+"'"
+                newUrl = url.replace(paramStr,paramExploitStr)
+                self.checkSqli(newUrl)
+
+                #Check for sql using "
+                paramExploitStr = paramStr+'"'
+                newUrl = url.replace(paramStr,paramExploitStr)
+                self.checkSqli(newUrl)
+            except:
+                pass
+
+    def checkSqli(self,url):
+        r = requests.get(url,verify=False,timeout=10)
+        html = r.text
+        
+        for regg in self.MySQL:
+            if(re.search(regg, html)): print(colored("[Vulnerable][MySql]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.PostgreSQL: 
+            if(re.search(regg, html)): print(colored("[Vulnerable][PostgreSQL]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.MicrosoftSQLServer:
+            if(re.search(regg, html)): print(colored("[Vulnerable][MicrosoftSQLServer]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.MicrosoftAccess:
+            if(re.search(regg, html)): print(colored("[Vulnerable][MicrosoftAccess]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.Oracle:
+            if(re.search(regg, html)): print(colored("[Vulnerable][Oracle]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.IBMDB2:
+            if(re.search(regg, html)): print(colored("[Vulnerable][IBMDB2]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.SQLite:
+            if(re.search(regg, html)): print(colored("[Vulnerable][SQLite]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
+        
+        for regg in self.Sybase:
+            if(re.search(regg, html)): print(colored("[Vulnerable][Sybase]\t" , 'green' , attrs=['bold']) , colored(url , 'red' , attrs=['bold']));return
